5619b2947533000e0f42099a02a8c63bd0495e17
David P. Smith <kcculhwch@gmail.com>
2020-09-25 23:18:16 -0400

    try loggeR

--- CoffeeGui/coffee_gui/src/main.rs
+++ CoffeeGui/coffee_gui/src/main.rs
@@ -2,12 +2,13 @@
 extern crate lovett;
 extern crate serde;
 extern crate bincode;
-
+extern crate env_logger;
 mod app;
 
 use app::*;
 
-fn main()  { 
+fn main()  {
+    env_logger::init(); 
     let app = App::new();
     run_app(app);
 }
