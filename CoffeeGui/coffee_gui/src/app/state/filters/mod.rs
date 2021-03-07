473c010e103b4795f9d51cbb3a05d353118a9b48
David P. Smith <kcculhwch@gmail.com>
2021-03-06 22:34:49 -0500

    track down temp set

--- CoffeeGui/coffee_gui/src/app/state/filters/mod.rs
+++ CoffeeGui/coffee_gui/src/app/state/filters/mod.rs
@@ -8,10 +8,10 @@ pub const BAR_VIEW_FILTER: StateSenderFilter = |old_state, new_state| {
 pub const SETTINGS_VIEW_FILTER: StateSenderFilter = |old_state, new_state| {
     let (old_state_decoded, new_state_decoded) = decode_states(old_state, new_state);
             old_state_decoded.settings.running != new_state_decoded.settings.running 
+            || old_state_decoded.settings.target_temp != new_state_decoded.settings.target_temp
             || old_state_decoded.settings.p != new_state_decoded.settings.p
             || old_state_decoded.settings.i != new_state_decoded.settings.i
             || old_state_decoded.settings.d != new_state_decoded.settings.d
-            || old_state_decoded.time.current_time != new_state_decoded.time.current_time 
 
 };
 
