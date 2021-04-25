c601646b50859d1bcef05d5ff2bfe544c196efd5
David P. Smith <kcculhwch@gmail.com>
2021-04-25 16:43:30 -0400

    update button position

--- CoffeeGui/coffee_gui/src/app/views/settings.rs
+++ CoffeeGui/coffee_gui/src/app/views/settings.rs
@@ -59,28 +59,28 @@ pub fn create(store: &mut Store) -> View {
     let mut settings_view = View::new( settings_update_fn, state_rx);
 
     // add buttons
-    let up_temp: Box<Button>            = Box::new(Button::new("▲".to_string(), 0, 36, 30, 16, 9, 1, 15, Event::new("[temp.click]", Some(vec!["up".to_string()])), &CONFIG)); 
-    let dn_temp: Box<Button>            = Box::new(Button::new("▼".to_string(), 0, 54, 30, 16, 9, -1, 15, Event::new("[temp.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
+    let up_temp: Box<Button>            = Box::new(Button::new("▲".to_string(), 0, 36, 31, 16, 9, 1, 15, Event::new("[temp.click]", Some(vec!["up".to_string()])), &CONFIG)); 
+    let dn_temp: Box<Button>            = Box::new(Button::new("▼".to_string(), 0, 54, 31, 16, 9, -1, 15, Event::new("[temp.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
     let temp_disp: Box<TextBlock>       = Box::new(TextBlock::new("200".to_string(), 25, 45, 80, 32, Event::new("[temp]", None), &CONFIG));
     let temp_label: Box<TextBlock>      = Box::new(TextBlock::new("°F : Target".to_string(), 90, 45, 100, 32, Event::new("[temp_label]", None), &CONFIG));
 
     // add p 
-    let up_p: Box<Button>               = Box::new(Button::new("▲".to_string(), 0, 90, 30, 16, 9, 1, 15, Event::new("[p.click]", Some(vec!["up".to_string()])), &CONFIG)); 
-    let dn_p: Box<Button>               = Box::new(Button::new("▼".to_string(), 0, 111, 30, 16, 9, -1, 15, Event::new("[p.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
+    let up_p: Box<Button>               = Box::new(Button::new("▲".to_string(), 0, 90, 31, 16, 9, 1, 15, Event::new("[p.click]", Some(vec!["up".to_string()])), &CONFIG)); 
+    let dn_p: Box<Button>               = Box::new(Button::new("▼".to_string(), 0, 111, 31, 16, 9, -1, 15, Event::new("[p.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
     let p_disp: Box<TextBlock>          = Box::new(TextBlock::new("15.0".to_string(), 25, 95, 80, 32, Event::new("[p]", None), &CONFIG));
     let p_label: Box<TextBlock>         = Box::new(TextBlock::new(" : P".to_string(), 90, 95, 100, 32, Event::new("[p_label]", None), &CONFIG));
 
     // add i
-    let up_i: Box<Button>               = Box::new(Button::new("▲".to_string(), 0, 140, 30, 16, 9, 1, 15, Event::new("[i.click]", Some(vec!["up".to_string()])), &CONFIG)); 
-    let dn_i: Box<Button>               = Box::new(Button::new("▼".to_string(), 0, 161, 30, 16, 9, -1, 15, Event::new("[i.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
+    let up_i: Box<Button>               = Box::new(Button::new("▲".to_string(), 0, 140, 31, 16, 9, 1, 15, Event::new("[i.click]", Some(vec!["up".to_string()])), &CONFIG)); 
+    let dn_i: Box<Button>               = Box::new(Button::new("▼".to_string(), 0, 161, 31, 16, 9, -1, 15, Event::new("[i.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
     let i_disp: Box<TextBlock>          = Box::new(TextBlock::new("3.0".to_string(), 25, 145, 80, 32, Event::new("[i]", None), &CONFIG));
     let i_label: Box<TextBlock>         = Box::new(TextBlock::new(" : I".to_string(), 90, 145, 100, 32, Event::new("[i_label]", None), &CONFIG));
 
 
 
     // add d
-    let up_d: Box<Button>               = Box::new(Button::new("▲".to_string(), 0, 190, 30, 16, 9, 1, 15, Event::new("[d.click]", Some(vec!["up".to_string()])), &CONFIG)); 
-    let dn_d: Box<Button>               = Box::new(Button::new("▼".to_string(), 0, 211, 30, 16, 9, -1, 15, Event::new("[d.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
+    let up_d: Box<Button>               = Box::new(Button::new("▲".to_string(), 0, 190, 31, 16, 9, 1, 15, Event::new("[d.click]", Some(vec!["up".to_string()])), &CONFIG)); 
+    let dn_d: Box<Button>               = Box::new(Button::new("▼".to_string(), 0, 211, 31, 16, 9, -1, 15, Event::new("[d.click]", Some(vec!["dn".to_string()])), &CONFIG)); 
     let d_disp: Box<TextBlock>          = Box::new(TextBlock::new("3.0".to_string(), 25, 195, 80, 32, Event::new("[d]", None), &CONFIG));
     let d_label: Box<TextBlock>         = Box::new(TextBlock::new(" : D".to_string(), 90, 195, 100, 32, Event::new("[d_label]", None), &CONFIG));
 
