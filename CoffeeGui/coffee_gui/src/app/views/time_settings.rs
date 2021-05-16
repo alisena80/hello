5b025cf2f1364e9a1fb26de6dfd607912f0b629c
David P. Smith <kcculhwch@gmail.com>
2021-05-15 22:32:22 -0400

    test debug

--- CoffeeGui/coffee_gui/src/app/views/time_settings.rs
+++ CoffeeGui/coffee_gui/src/app/views/time_settings.rs
@@ -11,25 +11,22 @@ use std::sync::mpsc::*;
 #[allow(dead_code, unused_variables)]
 pub fn create(store: &mut Store) -> View {
 
-
     // create the settings view
     // - since its the first view its also the home view
     let settings_update_fn: ViewStateUpdater = | objects, state, canvas | {
         let decoded_state: State = super::super::state_decoder(state);
-
-
     };
-
 
     let (state_tx, state_rx) = channel();
     store.reg_state_sender(state_tx, SETTINGS_VIEW_FILTER);
 
-    let mut settings_view = View::new( settings_update_fn, state_rx);
+    let mut settings_view = View::new(settings_update_fn, state_rx);
 
     // add label
     let page_label: Box<TextBlock>      = Box::new(TextBlock::new("Time Settings".to_string(), 32, 30, 186, 24, Event::new("[settings_label]", None), &CONFIG));
     let page_left: Box<Button>          = Box::new(Button::new("◀".to_string(), 0, 33, 16, 22, 2, 3, 15, Event::new("[settings_pager.click]", Some(vec!["left".to_string()])), &CONFIG));
     let page_right: Box<Button>         = Box::new(Button::new("▶".to_string(), 223, 33, 16, 22, 1, 3, 15, Event::new("[settings_pager.click]", Some(vec!["right".to_string()])), &CONFIG));
+
     // pager
     settings_view.add_static_object(page_label);
     settings_view.add_object(page_left, 0, 0);
